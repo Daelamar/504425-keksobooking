ca53894661649489a449e176dabe3daf2b11e6e6
Daelamar <redfess@yandex.ru>
2018-06-22 00:20:50 +0300

    Сделал первое задание пятого модуля

--- js/map.js
+++ js/map.js
@@ -43,7 +43,12 @@ var RENT_VALUE = {
     placeholder: 0
   }
 };
-
+var PIN_MAP_RESTRICTION = {
+  coordsMinY: 130,
+  coordsMaxY: 630,
+  coordsMinX: 0,
+  coordsMaxX: 1140
+};
 
 var offers;
 var mapElement = document.querySelector('.map');
@@ -245,8 +250,8 @@ var enablePage = function () {
 
   mapPinMainLeft = mapPinMainElement.offsetLeft;
   mapPinMainTop = mapPinMainElement.offsetTop;
-  inputAddressLeft = Math.round(mapPinMainLeft - mapPinMainWidth / 2);
-  inputAddressTop = Math.round(mapPinMainTop - mapPinMainHeight - AFTER_ELEMENT_MAIN_PIN);
+  inputAddressLeft = Math.round(mapPinMainLeft + mapPinMainWidth / 2);
+  inputAddressTop = Math.round(mapPinMainTop + mapPinMainHeight + AFTER_ELEMENT_MAIN_PIN);
   advertAddressInputElement.value = inputAddressLeft + ', ' + inputAddressTop;
 
   createPins(offers);
@@ -323,6 +328,57 @@ mapElement.addEventListener('click', function (evt) {
 });
 mapPinMainElement.addEventListener('mouseup', enablePage);
 disableFields();
+
+mapPinMainElement.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    if ((mapPinMainElement.offsetTop - shift.y) > PIN_MAP_RESTRICTION.coordsMinY && (mapPinMainElement.offsetTop - shift.y) < PIN_MAP_RESTRICTION.coordsMaxY) {
+      mapPinMainElement.style.top = (mapPinMainElement.offsetTop - shift.y) + 'px';
+    }
+    if ((mapPinMainElement.offsetLeft - shift.x) > PIN_MAP_RESTRICTION.coordsMinX && (mapPinMainElement.offsetLeft - shift.x) < PIN_MAP_RESTRICTION.coordsMaxX) {
+      mapPinMainElement.style.left = (mapPinMainElement.offsetLeft - shift.x) + 'px';
+    }
+  };
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - upEvt.clientX,
+      y: startCoords.y - upEvt.clientY
+    };
+
+    startCoords = {
+      x: upEvt.clientX,
+      y: upEvt.clientY
+    };
+    mapPinMainElement.style.top = (mapPinMainElement.offsetTop - shift.y) + 'px';
+    mapPinMainElement.style.left = (mapPinMainElement.offsetLeft - shift.x) + 'px';
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
 
 offers = createAdvertisement(8);
 advertAddressInputElement.value = inputAddressLeft + ', ' + inputAddressTop;
